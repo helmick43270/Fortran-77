139e3a06ea218d93cb8581c21a2b976ccdf73af1
Patrick Helmick <b43caa27e0@outlook.com>
2020-10-02 15:02:26 -0500

    Added comments to code

--- newtest.f
+++ newtest.f
@@ -1,11 +1,43 @@
+C     Lines that begin with C or an asterisk in column 1
+C     are treated as comments and ignored by compiler.
+C     Fortran 77 source code is fixed form and all caps.
+C     Column 7 is where source code lines are located.
+C     Main procedure is a PROGRAM and is named.
+
       PROGRAM NEWTEST
+
+C     Declare A and B as double precision
+C     numeric variables, 15 digits.
+
       DOUBLE PRECISION A, B
+
+C     Write to device #6(standard output for gfortran).
+C     Statement labelled 201 defines content to write.
+
       WRITE (6, 201)
  201  FORMAT ('PLEASE ENTER A NUMBER: ')
+
+C     Read from device #5(standard input for
+C     gfortran) into A; asterisk indicates that
+C     input can be any acceptable numeric format.
+
       READ (5,*) A
+
+C     Call double precision function (starts with a D)
+C     for double precision variables.
+
       B = DSQRT(A)
+
+C     Output result to standard output.
+
       WRITE (6, 202) A, B
  202  FORMAT ('THE SQUARE ROOT OF A IS B', 2F15.5)
+
+C     Stop program execution.
+
       STOP
+
+C     End of source code.
+
       END
 
